b14f481f345e221353b5c5f79e2f500c9204a3a7
DulceGarcia21 <denisse.garcia.2005@gmail.com>
2026-05-24 00:31:30 -0600

    crear vistas ejs

--- src/routes/bicitaxiRoutes.js
+++ src/routes/bicitaxiRoutes.js
@@ -2,10 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index');
-});
-
 router.get('/dashboard', (req, res) => {
     res.render('dashboard');
 });
